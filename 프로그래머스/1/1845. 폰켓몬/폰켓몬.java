a4b7392aa06d17d72ba8ac5510809f4d0ff4c3c0
김진범 <119301068+bum0w0@users.noreply.github.com>
2024-07-01 18:49:01 +0900

    [level 1] Title: 폰켓몬, Time: 2.17 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,22 +2,19 @@ import java.util.HashSet;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        int len = nums.length / 2;
+        // HashSet을 사용하여 폰켓몬 종류의 중복을 제거
         HashSet<Integer> set = new HashSet<>();
-
-        for(int type : nums) {
-            set.add(type);
+        for (int num : nums) {
+            set.add(num);
         }
 
-        int size = set.size();
-
-        if(size >= len) {
-            answer = len;
-        } else {
-            answer = size;
-        }
-
-        return answer;
+        // 선택할 수 있는 폰켓몬의 수는 N/2
+        int maxSelection = nums.length / 2;
+        
+        // 폰켓몬 종류의 수
+        int numOfTypes = set.size();
+        
+        // 선택할 수 있는 폰켓몬 종류의 최대 개수는 maxSelection 또는 numOfTypes 중 작은 값
+        return Math.min(maxSelection, numOfTypes);
     }
 }
